8aea9eb76354d6d785614313c476ee37be53aea9
Rushikesh Kale <rushikeshkale18k@gmail.com>
2025-09-11 01:09:38 +0530

    Created primary components

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Container from "./container/container";
+import LogoutBtn from "./header/LogoutBtn";
+import Logo from "./Logo";
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}

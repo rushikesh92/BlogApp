ea1affed5912b8404e01fbad1776ddc6e812d2c5
Rushikesh Kale <rushikeshkale18k@gmail.com>
2025-09-14 22:12:40 +0530

    Logo change

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({width = '130px'}) {
   return (
-    <div className={`text-sky-200 font-bold text-2xl`}>BasedBlog</div>
+    <div className={`text-sky-200 font-bold text-2xl`}>BlogBit</div>
   )
 }
 

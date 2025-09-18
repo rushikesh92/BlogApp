41250c954db5c8a5a4893d2897edb341f1f54a7a
Rushikesh Kale <rushikeshkale18k@gmail.com>
2025-09-19 01:00:18 +0530

    Update: Added OTP verification using email while signing up
    Appwrite provides endpoints that supports sign-up using email OTP, implemented functionality that perform this function.

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -8,36 +8,83 @@ export class AuthService {
 
     constructor() {
         this.client
-        .setEndpoint(conf.appwriteUrl)
-        .setProject(conf.appwriteProjectId);
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
 
         this.account = new Account(this.client)
 
     }
 
-    //sign-up
-    async createAccount({ email, password, name }) {
+    //sign-up : without otp only password basedx
+    // async createAccount({ email, password, name }) {
+    //     try {
+
+    //         const userAccount = await this.account.create({
+    //             userId: ID.unique(),
+    //             email,
+    //             password,
+    //             name
+    //         })
+
+    //         if (userAccount) {
+    //             //login
+    //               await this.login({ email, password })
+    //         }
+
+    //         return userAccount;
+
+    //     } catch (error) {
+    //         console.log("appwrite service :: createAccount error :: ", error)
+    //     }
+    // }
+
+    //sign-up otp based 
+    //generate OTP
+    async requestOTP(email) {
         try {
+            return await this.account.createEmailToken(
+                ID.unique(),
+                email
+            )
+        }
+        catch (error) {
+            console.log("appwrite service :: requestOTP error :: ", error)
 
-            const userAccount = await this.account.create({
-                userId: ID.unique(),
-                email,
-                password,
-                name
-            })
-
-            if (userAccount) {
-                //login
-                  await this.login({ email, password })
-            }
-           
-            return userAccount;
+        }
+        return null;
+    }
+    //verify OTP
+    async verifyOTP(userId, otp) {
+        try {
+            return await this.account.createSession(userId,otp);
+        }
+        catch (error) {
+            console.log("appwrite service :: verifyOTP error :: ", error)
             
+        }
+        return null;
+    }
+    //setName
+    async setName(name){
+        try {
+            return await this.account.updateName(name);
         } catch (error) {
-            console.log("appwrite service :: createAccount error :: ", error)
+            console.log("appwrite service :: setName error :: ", error)
+            
         }
     }
+    //set password (requires active session)
+    async setPassword(password){
+        try {
+            const updatedUser =  await this.account.updatePassword(password);
+            return updatedUser;
+        } catch (error) {
+        console.log("appwrite service :: setPassword error :: ", error)
+        
+    }
+    return null;
 
+   }
     //login
     async login({ email, password }) {
         try {
@@ -47,15 +94,15 @@ export class AuthService {
             });
         } catch (error) {
             console.log("appwrite service :: login error :: ", error)
-            
+
         }
         return null;
     }
-    
-    
+
+
     //check if logged in
     async getCurrentUser() {
-        
+
         try {
             const user = await this.account.get();
             // Logged in
@@ -66,15 +113,15 @@ export class AuthService {
         }
         return null;
     }
-    
+
     //log-out
-    async logout(){
+    async logout() {
         try {
             const result = await this.account.deleteSessions();
-            
+
         } catch (error) {
             console.log("appwrite service :: logout error :: ", error)
-            
+
         }
     }
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,47 +4,96 @@ import { useDispatch } from 'react-redux'
 import { login as storeLogin } from '../store/authSlice'
 import { Button, Input, Select, Logo } from './index'
 import authService from '../appwrite/auth'
-import { set, useForm } from 'react-hook-form'
-function  Signup() {
+import { useForm } from 'react-hook-form'
+function Signup() {
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const {register, handleSubmit} = useForm();
+  const { register, handleSubmit } = useForm();
+
   const [error, setError] = useState("");
-    const [signupDisabled, setSignupDisabled] = useState(false);
+  const [step, setStep] = useState(1);
+  //step 1 => name & email input 2 => Otp verification 3 => set password 
+  const [userId, setUserId] = useState("");
+  const [otp, setOtp] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const [signupDisabled, setSignupDisabled] = useState(false);
+  const [verifyDisabled, setVerifyDisabled] = useState(false);
+  const [continueDisabled, setContinueDisabled] = useState(false);
+
 
   const signup = async (data) => {
     setError("");
     setSignupDisabled(true);
     try {
-      const userAccount = authService.createAccount(data);
+      const result = await authService.requestOTP(data.email);
 
-      if (userAccount) {
-        const userData = await authService.getCurrentUser();
-
-        if (userData) {
-          dispatch(storeLogin(userData))
-        }
-        navigate("/")
+      if (result) {
+        setUserId(result.userId);
+        setName(data.name);
+        setStep(2);
       }
+
     } catch (error) {
-      setError(error)
+      setError(error.message || "Something went wrong");
       setSignupDisabled(false);
     }
   }
+
+  const handleOTPSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setVerifyDisabled(true);
+    try {
+      const session = await authService.verifyOTP(userId, otp);
+      if (session) {
+        setStep(3);
+      }
+      else {
+        setError("OTP verification failed")
+        setVerifyDisabled(false);
+
+      }
+    } catch (error) {
+      setError(error.message || "Something went wrong");
+      setVerifyDisabled(false);
+
+    }
+  }
+  const handlePasswordSubmit = async (e) => {
+    setError("")
+    e.preventDefault();
+    setContinueDisabled(true);
+    try {
+      await authService.setName(name);
+      await authService.setPassword(password);
+      const userData = await authService.getCurrentUser();
+      if (userData) {
+        dispatch(storeLogin(userData));
+        navigate("/");
+      }
+    } catch (error) {
+      setError(error.message || "Something went wrong");
+      setContinueDisabled(false);
+    }
+  };
+
   return (
-    <div
-      className='flex items-center justify-center w-full'
-    >
-      <div className={`mx-auto w-full max-w-lg bg-gray-500/30 rounded-xl p-10 border border-black/10`}>
+
+    <div className="flex items-center justify-center w-full">
+      <div className="mx-auto w-full max-w-lg bg-gray-500/30 rounded-xl p-10 border border-black/10">
         <div className="mb-2 flex justify-center">
           <span className="inline-block w-full max-w-[100px]">
             <Logo width="100%" />
           </span>
         </div>
-        <h2 className="text-center text-2xl font-bold leading-tight">Create new account</h2>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Create new account
+        </h2>
         <p className="mt-2 text-center text-base text-white/70">
-          Already have a account?&nbsp;
+          Already have an account?&nbsp;
           <Link
             to="/login"
             className="font-medium text-primary transition-all duration-200 hover:underline"
@@ -52,55 +101,84 @@ function  Signup() {
             Log In
           </Link>
         </p>
+
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
 
-        <form onSubmit={handleSubmit(signup)} className='mt-8'>
-          <div className="space-y-5 mt-2">
-            <Input
-              label='Name: '
-              placeholder='Enter your name'
-              type='text'
-              {...register('name', {
-                required: true,
-              })
-              }
-            />
-          </div>
-          <div className="space-y-5 mt-2">
-            <Input
-              label='Email: '
-              placeholder='Enter your email'
-              type='email'
+        {step === 1 && (
+          <form onSubmit={handleSubmit(signup)} className="mt-8">
+            <div className="space-y-5 mt-2">
+              <Input
+                label="Name:"
+                placeholder="Enter your name"
+                type="text"
+                {...register("name", { required: true })}
+              />
+            </div>
+            <div className="space-y-5 mt-2">
+              <Input
+                label="Email:"
+                placeholder="Enter your email"
+                type="email"
+                {...register("email", {
+                  required: true,
+                  validate: {
+                    matchPattern: (value) =>
+                      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value) ||
+                      "Email address must be valid",
+                  },
+                })}
+              />
+            </div>
+            <Button
+              type="submit"
+              className="w-full"
+              isDisabled={signupDisabled}
+            >
+              Create Account
+            </Button>
+          </form>
+        )}
 
-              //using react-hook-form
-              {...register('email', {
-                required: true,
-                validate: {
-                  matchPattern: (value) => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-                    .test(value) || "Email address must be valid"
-                }
-              })
-              }
-            />
-          </div>
-          <div className="space-y-5 mt-2">
+        {step === 2 && (
+          <form onSubmit={handleOTPSubmit} className="mt-4">
+            <p className="text-sm text-white/70 text-center mb-4">
+              Enter the OTP sent to your email
+            </p>
             <Input
-              label='Password: '
-              placeholder='Enter your password'
-              type='password'
-              {...register('password', {
-                required: true,
-              })
-              }
+              // label="OTP:"
+              placeholder="Enter the 6-digit code"
+              type="text"
+              value={otp}
+              onChange={(e) => setOtp(e.target.value)}
             />
-          </div>
-          <Button type='submit' className='w-full' isDisabled={signupDisabled}>Create Account</Button>
-        </form>
+            <Button type="submit" className="w-full mt-4" isDisabled={verifyDisabled}>
+              Verify OTP
+            </Button>
+          </form>
+        )}
 
+        {step === 3 && (
+          <form onSubmit={handlePasswordSubmit} className="mt-4">
+            <p className="text-sm text-white/70 text-center mb-4">
+              Set your password 
+            </p>
+            <Input
+              label="Password:"
+              placeholder="Enter your password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" className="w-full mt-4" isDisabled={continueDisabled}>
+              Continue
+            </Button>
+          </form>
+        )}
       </div>
-
     </div>
-  )
+  );
+
+
 }
 
 export default Signup
